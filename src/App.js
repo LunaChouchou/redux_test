c4f506a8fba588ef158c346500254fb57b3ba325
Luna Chouchou <liu2shu1@outlook.com>
2023-11-22 18:07:50 +0900

    src差分確認用_1_src_纯react版

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Count from './components/Count'
 
 export default class App extends Component {
   render() {
     return (
-      <div>App...</div>
+      <div>
+        <Count/>
+      </div>
     )
   }
 }
