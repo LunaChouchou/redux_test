0166f0158a05d34c7d5b55b34a644b5a75c082e2
Luna Chouchou <liu2shu1@outlook.com>
2023-11-23 19:33:53 +0900

    src差分確認用_5_src_react-redux的基本使用（yarn add react-redux）

--- src/components/Count/index.jsx
+++ src/components/Count/index.jsx
@@ -1,57 +1,36 @@
 import React, { Component } from 'react'
-//引入store
-import store from '../../redux/store'
-//引入actionCreater，专门用于创建action对象
-import {
-  createIncrementAction,
-  createDecrementAction,
-  createIncrementAsyncAction
-} from '../../redux/count_action'
 
 export default class Count extends Component {
 
     state = {carName:'奔驰c63'}
 
-/*
-    componentDidMount(){
-      //检测redux中状态的变化，只要变化，就调用render
-      store.subscribe(() => {
-        //this.render() 没用
-        this.setState({})
-      })
-    }
-*/
-
     //加法
     increment = () => {
       const {value} = this.selectedNumber
-      store.dispatch(createIncrementAction(value*1))
+      this.props.jia(value*1)
     }
     //减法
     decrement = () => {
       const {value} = this.selectedNumber
-      store.dispatch(createDecrementAction(value*1))
+      this.props.jian(value*1)
     }
     //奇数再加
     incrementIfOdd = () => {
         const {value} = this.selectedNumber
-        const count = store.getState()
-        if(count % 2 !== 0) {
-          store.dispatch(createIncrementAction(value*1))
+        if (this.props.count % 2 !==0) {
+          this.props.jia(value*1)
         }
     }
     //异步加（等一会儿加）
     incrementAsync = () => {
         const {value} = this.selectedNumber
-        // setTimeout(()=>{
-          store.dispatch(createIncrementAsyncAction(value*1,500))
-        // },500)
+        this.props.jiaAsync(value*1,500)
     }
 
   render() {
     return (
       <div>
-        <h1>当前求和为：{store.getState()}</h1>
+        <h1>当前求和为：{this.props.count}</h1>
         <select ref={c => this.selectedNumber = c}>
             <option value="1">1</option>
             <option value="2">2</option>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from './redux/store'
 
 createRoot(document.getElementById('root')).render(<App/>)
 
+//检测redux中状态的改变，如果redux的状态发生了改变，那么重新渲染App组件
 store.subscribe(() => {
     createRoot(document.getElementById('root')).render(<App/>)
 })
